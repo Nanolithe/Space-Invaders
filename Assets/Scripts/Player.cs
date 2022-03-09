843aa1b5a304a966a2895356fdca9c2ed565ea03
Nanolithe <Kevin3320977@gmail.com>
2022-03-08 22:34:24 -0800

    Space Invaders 3/8/2022

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,8 +5,9 @@ public class Player : MonoBehaviour
 {
     [FormerlySerializedAs("bullet")] public GameObject bulletPrefab;
     [FormerlySerializedAs("shottingOffset")] public Transform shootOffsetTransform;
-
+    private float movementPerSecond = 10f;
     private Animator playerAnimator;
+    private static readonly int Shoot = Animator.StringToHash("Shoot");
 
     //-----------------------------------------------------------------------------
     void Start()
@@ -20,10 +21,25 @@ public class Player : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             // todo - trigger a "shoot" on the animator
+            playerAnimator.SetTrigger("Shoot");
             GameObject shot = Instantiate(bulletPrefab, shootOffsetTransform.position, Quaternion.identity);
             Debug.Log("Bang!");
 
             Destroy(shot, 3f);
         }
+        
+        float movementAxis = Input.GetAxis("Horizontal");
+        Transform transform = GetComponent<Transform>();
+        transform.position += Vector3.right * movementAxis * movementPerSecond * Time.deltaTime;
+
+        Vector3 force = Vector3.right * movementAxis * movementPerSecond * Time.deltaTime;
+
+        Rigidbody rbody = GetComponent<Rigidbody>();
+        if (rbody)
+        {
+            rbody.AddForce(force, ForceMode.VelocityChange);
+        }
+
+         transform.position += Vector3.right * movementAxis * movementPerSecond * Time.deltaTime;
     }
 }
---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int score = 0000;
+    public int hiScore = 0000;
+
+    //public TMP_Text;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

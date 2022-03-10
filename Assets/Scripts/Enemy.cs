e9bacd9b7f5516e895135d7d82762e9043b8704c
Nanolithe <Kevin3320977@gmail.com>
2022-03-09 23:43:42 -0800

    Space Invaders 3/9/2022

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,10 +2,8 @@
 
 public class Enemy : MonoBehaviour
 {
-    //-----------------------------------------------------------------------------
-    void OnCollisionEnter2D(Collision2D collision)
+    void OnTriggerEnter2D(Collider2D collision)
     {
-        // todo - trigger death animation
         Destroy(collision.gameObject); // destroy bullet
         Debug.Log("Ouch!");
     }
